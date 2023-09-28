81fbe184345eaf936e883184b577ead7e488ec1f
HorneOnne <anhlaquyax@gmail.com>
2023-09-28 19:50:11 +0700

    Add Card Data

---
+++ Assets/Dial21/Scripts/CardData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Dial21
+{
+    [CreateAssetMenu()]
+    public class CardData : ScriptableObject
+    {
+        public Sprite CardSprite;
+        public int CardValue;
+    }
+}
